9b9e7d5b700b6dd39ceafa962b121277e21e62f6
Elliott Jones <jones.elliott@outlook.com>
2020-07-25 09:57:17 -0700

    Set variables in JS file referencing HTML elements

---
+++ script.js
@@ -0,0 +1,7 @@
+// Declaring variables referencing HTML elements
+var pageTitle = document.querySelector('#title-element');
+var timerDiv = document.querySelector('#timer-div');
+var headerDiv = document.querySelector('#header-div');
+var contentDiv1 = document.querySelector('#content-1-div');
+var contentDiv2 = document.querySelector('#content-2-div');
+var buttonsDiv = document.querySelector('#buttons-div');
